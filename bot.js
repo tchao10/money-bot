bc96e466fb5554f97c9c4f81993f4335e4a403b1
tchao10 <37792003+tchao10@users.noreply.github.com>
2018-03-26 00:28:06 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,13 +8,20 @@ bot.on('ready', () => {
 });
 
 bot.on('message', message => {
-	switch(message.content){
-		case 'ping':
-			message.reply('dong');
-		break;
-		case '$donate':
-			message.reply('thanks, I have $'+moneyCount+' now!');
-		break;
+	if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+	if (message.content === '$donate') {
+		moneyCount++;
+    	message.reply('thanks, I have $'+moneyCount+' now!');
+  	}
+	//switch(message.content){
+	//	case 'ping':
+	//		message.reply('dong');
+	//	break;
+	//	case '$donate':
+	//		message.reply('thanks, I have $'+moneyCount+' now!');
+	//	break;
 });
 
 bot.login(process.env.BOT_TOKEN);
