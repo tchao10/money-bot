07676d81ff93fbde02d1038b8e92bd7c559422d3
tchao10 <37792003+tchao10@users.noreply.github.com>
2018-03-26 00:49:26 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,8 @@ bot.on('message', message => {
   	}
 	if (message.content === '$donate') {
 		moneyCount++;
-    	message.reply('thanks, I have $'+moneyCount+' now! :musicteacher:');
+    	message.reply('thanks, I have $'+moneyCount+' now! \:musicteacher:');
+		//message.react(message.guild.emojis.get('123456789012345678'));
   	}
 	//switch(message.content){
 	//	case 'ping':
