5e79a0438fc7360d9ca9a4449f0c06582815627b
tchao10 <37792003+tchao10@users.noreply.github.com>
2018-03-26 00:01:50 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,20 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
+var moneyCount = 0;
+
 bot.on('ready', () => {
     console.log('I am ready!');
 });
 
 bot.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('dong');
-  	}
+	switch(message.content){
+		case 'ping':
+			message.reply('dong');
+		break;
+		case '$donate':
+			message.reply('thanks, I have $'+moneyCount+' now!');
+		break;
 });
 
 bot.login(process.env.BOT_TOKEN);
