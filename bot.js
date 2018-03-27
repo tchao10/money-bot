4eeb69cab1bdf502c25e51ca440435a0506c1b3e
tchao10 <37792003+tchao10@users.noreply.github.com>
2018-03-26 22:48:52 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,20 +14,20 @@ bot.on('message', message => {
 	
 	// It's good practice to ignore other bots. This also makes your bot ignore itself
 	// and not get into a spam loop (we call that "botception").
-	//if(message.author.bot)
-	//	return;
+	if(message.author.bot)
+		return;
 		
 	// Also good practice to ignore any message that does not start with our prefix, 
 	// which is set in the configuration file.
-	//if(message.content.indexOf(this.messagePrefix) !== 0)
-	//	return;
+	if(message.content.indexOf(this.messagePrefix) !== 0)
+		return;
 	
 	// Here we separate our "command" name, and our "arguments" for the command. 
 	// e.g. if we have the message "+say Is this the real life?" , we'll get the following:
 	// command = say
 	// args = ["Is", "this", "the", "real", "life?"]
-	//const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-	//const command = args.shift().toLowerCase();
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
 	
 	//===== My Commands (edited) =================================================================
 	
@@ -44,13 +44,6 @@ bot.on('message', message => {
 		message.react(message.guild.emojis.get('426956349751164950'));
     	message.reply('thanks, I have $'+moneyCount+' now!');
   	}
-	//switch(message.content){
-	//	case 'ping':
-	//		message.reply('dong');
-	//	break;
-	//	case '$donate':
-	//		message.reply('thanks, I have $'+moneyCount+' now!');
-	//	break;
 });
 
 bot.login(process.env.BOT_TOKEN);
