7d2c4268b2c686a868ac4c9b37086c0fb0d599c0
Glitch (moneymoneymoney-bot) <none>
2020-08-15 04:18:30 +0000

    👾🌄 Checkpoint ./bot.js:1289277/1629 ./watch.json:1289277/4

--- bot.js
+++ bot.js
@@ -125,9 +125,27 @@ client.on('message', message => {
 
 	if (command === 'uptime'){
 		// Get time since restartTime
+		var currentDate = new Date();
 
+		// To supposedly handle times that are on opposite sides of midnight e.g. 9pm and 5am
+		if (currentDate < restartTime){
+			currentDate.setDate(currentDate.getDate() + 1);
+		}
 
-		message.channel.send("The bot was last restarted at **" + restartTime + "**.");
+		var differenceBetweenCurrentAndRestartTimes = currentDate - restartTime;
+
+		var msec = differenceBetweenCurrentAndRestartTimes;
+		var hh = Math.floor(msec / 1000 / 60 / 60);
+		msec -= hh * 1000 * 60 * 60;
+		var mm = Math.floor(msec / 1000 / 60);
+		msec -= mm * 1000 * 60;
+		var ss = Math.floor(msec / 1000);
+		msec -= ss * 1000;
+
+		var restartTimeLocaleString = restartTime.toLocaleString("en-US", {timeZone: "America/Los_Angeles",});
+
+		message.channel.send("__Uptime__: " + hh + " hours, " + mm + " minutes, " + ss + " seconds, and " + msec + " milliseconds");
+		message.channel.send("__Last Restart time__: " + restartTimeLocaleString);
 	}
 	
 	//shotgun related stuff ===========================================================
