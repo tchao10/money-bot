abc4455e0841387c4efd8052f152ffb9d8b3232c
Glitch (moneymoneymoney-bot) <none>
2020-08-21 23:16:53 +0000

    ☄️🍣 Checkpoint ./commands/donate.js:1289277/12 ./commands/help.js:1289277/217

--- commands/donate.js
+++ commands/donate.js
@@ -1,12 +1,12 @@
 module.exports = {
-	name: 'donate',
-	description: 'Donates money to me.',
+	name: "donate",
+	description: "Donates money to me.",
 	usage: "No usage provided.",
 	aliases: [],
 	args: false,
 	guildOnly: false,
 	execute(message, args) {
-		//message.react(message.guild.emojis.get('426956349751164950'));
+		//message.react(message.guild.emojis.get("426956349751164950"));
         message.reply("Need to move donate to `donate.js`");
 	},
 };
