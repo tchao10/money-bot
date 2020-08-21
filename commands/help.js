845ae30f3b8bfc1f8f65efab502277524329f4d3
Glitch (moneymoneymoney-bot) <none>
2020-08-21 07:03:03 +0000

    🏘🌁 Checkpoint ./commands/help.js:1289277/958

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,5 @@
+const { prefix } = require("./config.json");
+
 module.exports = {
 	name: 'help',
 	description: 'Displays a list of commands.',
@@ -6,6 +8,27 @@ module.exports = {
 	args: false,
 	guildOnly: false,
 	execute(message, args) {
-        message.reply("here is a list of commands: $help, $ping, $donate, $uno, $weather, $pls meme, $coinflip, $spamunobot, $play <songName/URL>, $leave, $shotgun, $uptime");
+        //message.reply("here is a list of commands: $help, $ping, $donate, $uno, $weather, $pls meme, $coinflip, $spamunobot, $play <songName/URL>, $leave, $shotgun, $uptime");
+	
+		const data = [];
+		const { commands } = message.client;
+
+		if (!args.length) {
+			data.push('Here\'s a list of all my commands:');
+			data.push(commands.map(command => command.name).join(', '));
+			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+
+			return message.author.send(data, { split: true })
+				.then(() => {
+					if (message.channel.type === 'dm'){
+						return;
+					}
+					message.reply('I\'ve sent you a DM with all my commands!');
+				})
+				.catch(error => {
+					console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
+					message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
+				});
+		}
 	},
 };
