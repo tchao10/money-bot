60653d7d72d893113a1e6a0d944d32c0ec5eb92d
Glitch (moneymoneymoney-bot) <none>
2020-08-21 07:37:18 +0000

    ⛈👰 Checkpoint ./commands/reload.js:1289277/685

---
+++ commands/reload.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: "reload",
+	description: "Reloads the bot commands.",
+	usage: "No usage provided.",
+	aliases: [],
+	args: false,
+	guildOnly: false,
+	execute(message, args) {
+		
+	},
+};
