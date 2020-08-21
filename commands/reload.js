02e78ee2e8adcdf797a3b3d3fe86ade6428357f0
Glitch (moneymoneymoney-bot) <none>
2020-08-21 07:59:25 +0000

    🏜🎣 Checkpoint ./commands/reload.js:1289277/1070

--- commands/reload.js
+++ commands/reload.js
@@ -1,9 +1,11 @@
+// https://discordjs.guide/command-handling/adding-features.html#reloading-commands
+
 module.exports = {
 	name: "reload",
-	description: "Reloads the bot commands.",
+	description: "Reloads a command.",
 	usage: "No usage provided.",
-	aliases: [],
-	args: false,
+	aliases: ["rl"],
+	args: true,
 	guildOnly: false,
 	execute(message, args) {
 		if (!args.length){
@@ -12,6 +14,19 @@ module.exports = {
         const commandName = args[0].toLowerCase();
         const command = message.client.commands.get(commandName) || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
-        if (!command) return message.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`);
-	},
+        if (!command){
+            return message.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`);
+        }
+    
+        delete require.cache[require.resolve(`./${command.name}.js`)];
+
+        try {
+            const newCommand = require(`./${command.name}.js`);
+            message.client.commands.set(newCommand.name, newCommand);
+            message.channel.send(`Command \`${command.name}\` was reloaded!`);
+        } catch (error) {
+            console.log(error);
+            message.channel.send(`There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``);
+        }
+    },
 };
