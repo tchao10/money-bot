8c71405989b7111eb06ef0df4e28addbb73c826d
Glitch (moneymoneymoney-bot) <none>
2020-08-21 02:16:22 +0000

    🛋👔 Checkpoint ./commands/uptime.js:1289277/192

--- commands/uptime.js
+++ commands/uptime.js
@@ -2,17 +2,17 @@ module.exports = {
 	name: 'uptime',
 	description: 'Uptime',
 	execute(message, args) {
-		// Get time since restartTime
+		// Get time since botIsLiveTime
 		var currentDate = new Date();
 
 		// To supposedly handle times that are on opposite sides of midnight e.g. 9pm and 5am
-		if (currentDate < restartTime){
+		if (currentDate < botIsLiveTime){
 			currentDate.setDate(currentDate.getDate() + 1);
 		}
 
-		var differenceBetweenCurrentAndRestartTimes = currentDate - restartTime;
+		var differenceBetweenCurrentAndbotIsLiveTimes = currentDate - botIsLiveTime;
 
-		var msec = differenceBetweenCurrentAndRestartTimes;
+		var msec = differenceBetweenCurrentAndbotIsLiveTimes;
 		var hh = Math.floor(msec / 1000 / 60 / 60);
 		msec -= hh * 1000 * 60 * 60;
 		var mm = Math.floor(msec / 1000 / 60);
@@ -20,8 +20,8 @@ module.exports = {
 		var ss = Math.floor(msec / 1000);
 		msec -= ss * 1000;
 
-		var restartTimeLocaleString = restartTime.toLocaleString("en-US", {timeZone: "America/Los_Angeles",});
+		var botIsLiveTimeLocaleString = botIsLiveTime.toLocaleString("en-US", {timeZone: "America/Los_Angeles",});
 
-		message.channel.send("**Uptime**: " + hh + " hours, " + mm + " minutes, " + ss + " seconds, and " + msec + " milliseconds\n**Last restart time**: " + restartTimeLocaleString + " PST");
+		message.channel.send("**Uptime**: " + hh + " hours, " + mm + " minutes, " + ss + " seconds, and " + msec + " milliseconds\n**Last restart time**: " + botIsLiveTimeLocaleString + " PST");
 	},
 };
