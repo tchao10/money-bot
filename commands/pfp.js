0f6c258faec9681a1c1a3180d84dd836026a5b55
Glitch (moneymoneymoney-bot) <none>
2020-08-22 04:42:27 +0000

    👦🍛 Checkpoint ./commands/pfp.js:1289277/738 ./watch.json:1289277/2

--- commands/pfp.js
+++ commands/pfp.js
@@ -6,14 +6,8 @@ module.exports = {
 	arguments: false,
 	guildOnly: false,
 	execute(message, arguments){
-		var randNum = Math.random();
-    
-		if (randNum < 0.45){
-			message.channel.send("Heads.");
-		} else if (randNum < 0.55){
-			message.channel.send("oops i dropped the coin");
-		} else {
-			message.channel.send("Tails.");
+		if (!arguments.size){
+			return message.channel.send(`Your avatar: <${message.author.avatarURL({ format: "png", dynamic: true })}>`);
 		}
 	},
 };
