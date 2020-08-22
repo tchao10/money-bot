f52f98724a95f0c7f7956d02c1ee79f73030ea49
Glitch (moneymoneymoney-bot) <none>
2020-08-22 00:02:57 +0000

    🎹🦄 Checkpoint ./index.js:1289277/2 ./commands/leave.js:1289277/30

--- commands/leave.js
+++ commands/leave.js
@@ -1,12 +1,12 @@
 module.exports = {
-	name: 'leave',
-	description: 'Makes the bot leave the voice channel.',
-	usage: "No usage provided.",
+	name: "leave",
+	description: "Makes the bot leave the voice channel.",
+	usage: "",
 	aliases: [],
 	args: false,
 	guildOnly: true,
 	execute(message, args) {
         message.member.voiceChannel.leave();
-		message.channel.send('bye');
+		message.channel.send("bye");
 	},
 };
