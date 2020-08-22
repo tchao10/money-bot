d99d0142cf7931d3bb8f4dd66a58abd6f05f6de5
Glitch (moneymoneymoney-bot) <none>
2020-08-22 01:23:00 +0000

    🍁💓 Checkpoint ./commands/help.js:1289277/275

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ const { prefix } = require("../config.json");
 module.exports = {
 	name: "help",
 	description: "Displays a list of commands.",
-	usage: "No usage provided.",
+	usage: "",
 	aliases: ["commands"],
 	arguments: false,
 	guildOnly: false,
@@ -13,9 +13,12 @@ module.exports = {
 
 		if (!arguments.length) {
 			data.push("Here's a list of all my commands:");
-			data.push(commands.map(command => command.name).join(", "));
+			data.push("`" + commands.map(command => command.name).join("`, `") + "`");
 			data.push("\nYou can send `" + prefix + "help [command name]` to get info on a specific command!");
 
+			return message.channel.send(data);
+
+			/* Sends help message via DM
 			return message.author.send(data, { split: true })
 				.then(() => {
 					if (message.channel.type === "dm"){
@@ -27,6 +30,7 @@ module.exports = {
 					console.error("Could not send help DM to " + message.author.tag + ".\n", error);
 					message.reply("it seems like I can't DM you! Do you have DMs disabled?");
 				});
+			*/
 		}
 
 		const name = arguments[0].toLowerCase();
