86eb3cc26a743371e5d212d84b46c9f8789cb960
Glitch (moneymoneymoney-bot) <none>
2020-08-24 00:48:05 +0000

    ☄️🌑 Checkpoint ./commands/shotgun.js:1289277/360

--- commands/shotgun.js
+++ commands/shotgun.js
@@ -141,8 +141,12 @@ module.exports = {
 			return;
 		}
 
-		// Display help message in every other case
-		return message.channel.send("Shotgun is a simple turn-based game versus <@374095302648659980>. There are three actions you can take each turn:\n\n`" + prefix + this.name + " shoot` shoots your gun if you have ammo.\n`" + prefix + this.name + " reload` adds one bullet to your gun.\n`" + prefix + this.name + " block` prevents you from taking damage that turn.\n\nThe first player to get their opponent's health to 0 wins! Start a game by typing `" + prefix + this.name + " start`.");
+		// Display help message
+		if (!arguments.length || arguments[0] == "help"){
+			return message.channel.send("Shotgun is a simple turn-based game versus <@374095302648659980>. There are three actions you can take each turn:\n\n`" + prefix + this.name + " shoot` shoots your gun if you have ammo.\n`" + prefix + this.name + " reload` adds one bullet to your gun.\n`" + prefix + this.name + " block` prevents you from taking damage that turn.\n\nThe first player to get their opponent's health to 0 wins! Start a game by typing `" + prefix + this.name + " start`.");
+		}
+
+		return;
 	},
 };
 
