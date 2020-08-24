a8f3a9d22daa08a8b3145606023de4fa7efad99c
Glitch (moneymoneymoney-bot) <none>
2020-08-24 03:29:14 +0000

    🕶🌯 Checkpoint ./commands/pfp.js:1289277/1356

--- commands/pfp.js
+++ commands/pfp.js
@@ -7,11 +7,17 @@ module.exports = {
 	guildOnly: false,
 	execute(message, arguments){
 		// If the first argument is a mentioned user, then show the mentioned user's profile picture
-		if (arguments.length && message.mentions.users.first() == arguments[0]){
+		/*
+		console.log(arguments.length);
+		console.log(message.mentions.users.first());
+		console.log(arguments[0]);
+		*/
+		console.log(message.mentions.users.first().id == arguments[0]);
+		if (arguments.length && message.mentions.users.first().id == arguments[0]){
 			return message.channel.send(message.mentions.users.first().displayAvatarURL({ format: "png", dynamic: true }));
 		}
 
 		// Otherwise, just show the author's profile picture (this also takes care of "garbage" arguments)
-		return message.author.displayAvatarURL({ format: "png", dynamic: true });
+		return message.channel.send(message.author.displayAvatarURL({ format: "png", dynamic: true }));
 	},
 };
