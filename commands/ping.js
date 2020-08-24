3e155bc3d2df50be485119dc8a097a46572b249c
Glitch (moneymoneymoney-bot) <none>
2020-08-24 01:23:12 +0000

    🍝🏢 Checkpoint ./commands/shotgun.js:1289277/39 ./commands/ping.js:1289277/1085

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+
 module.exports = {
 	name: "ping",
 	description: "pong",
@@ -7,5 +9,25 @@ module.exports = {
 	guildOnly: false,
 	execute(message, arguments){
 		message.reply("pong");
+
+		const exampleEmbed = new Discord.MessageEmbed()
+			.setColor('#0099ff')
+			.setTitle('Some title')
+			.setURL('https://discord.js.org/')
+			.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+			.setDescription('Some description here')
+			.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+			.addFields(
+				{ name: 'Regular field title', value: 'Some value here' },
+				{ name: '\u200B', value: '\u200B' },
+				{ name: 'Inline field title', value: 'Some value here', inline: true },
+				{ name: 'Inline field title', value: 'Some value here', inline: true },
+			)
+			.addField('Inline field title', 'Some value here', true)
+			.setImage('https://i.imgur.com/wSTFkRM.png')
+			.setTimestamp()
+			.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+		message.channel.send(exampleEmbed);
 	},
 };
--- commands/shotgun.js
+++ commands/shotgun.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js');
 const { prefix } = require("../config.json");
 
 module.exports = {
