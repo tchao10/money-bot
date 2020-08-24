5df0f0837d2f89d585394d9e997167b240193e92
Glitch (moneymoneymoney-bot) <none>
2020-08-24 06:25:06 +0000

    🎁🎂 Checkpoint ./commands/ping.js:1289277/780

--- commands/ping.js
+++ commands/ping.js
@@ -9,7 +9,7 @@ module.exports = {
 	arguments: false,
 	guildOnly: false,
 	execute(message, arguments){
-		message.reply("pong");
+		message.channel.send("pong");
 
 		if (message.author.id != 134095374381088768){ // If you are not me
             return;
@@ -30,5 +30,23 @@ module.exports = {
 			.setFooter(prefix + this.name + " help for instructions")
 
 		message.channel.send(exampleEmbed);
+
+		console.log(exampleEmbed);
+
+		const newEmbed = new Discord.MessageEmbed()
+			.setColor("#0099ff")
+			.setTitle("Shotgun")
+			.setDescription(message.author.toString() + " vs. <@374095302648659980>")
+			.setThumbnail("https://cdn.discordapp.com/avatars/374095302648659980/3953362a62cb6a1bdce66f13a31aef4a.png")
+			.addFields(
+				{ name: "Your health:", value: "Your ammo:", inline: true },
+				{ name: "Bot's health:", value: "Bot's ammo:", inline: true },
+				{ name: "\u200B", value: "\u200B" },
+				{ name: "Inline field title", value: "Some value here", inline: true },
+			)
+			.setTimestamp()
+			.setFooter(prefix + this.name + " help for instructions")
+
+		message.edit(newEmbe)
 	},
 };
