9b77c59ec64a60cc04bb5c67fedf2cd96b7f4ee4
Glitch (moneymoneymoney-bot) <none>
2020-08-26 07:21:43 +0000

    🍈🍉 Checkpoint ./commands/ping.js:1289277/1504

--- commands/ping.js
+++ commands/ping.js
@@ -15,43 +15,6 @@ module.exports = {
             return;
         }
 
-		var embedMessage;
-
-		const exampleEmbed = new Discord.MessageEmbed()
-			.setColor("#0099ff")
-			.setTitle("Shotgun")
-			.setDescription(message.author.toString() + " vs. <@374095302648659980>")
-			.setThumbnail("https://cdn.discordapp.com/avatars/374095302648659980/3953362a62cb6a1bdce66f13a31aef4a.png")
-			.addFields(
-				{ name: "Your health:", value: "Your ammo:", inline: true },
-				{ name: "Bot's health:", value: "Bot's ammo:", inline: true },
-				{ name: "\u200B", value: "\u200B" },
-				{ name: "Inline field title", value: "Some value here", inline: true },
-			)
-			.setTimestamp()
-			.setFooter(prefix + this.name + " help for instructions")
-
-		message.channel.send(exampleEmbed).then(sentMessage => {
-			embedMessage = sentMessage;
-		});
-
-		//console.log(exampleEmbed);
-		await new Promise(r => setTimeout(r, 1000));
-
-		const newEmbed = new Discord.MessageEmbed()
-			.setColor("#0099ff")
-			.setTitle("Shotgun asdasd")
-			.setDescription(message.author.toString() + " vs. <@374095302648659980>")
-			.setThumbnail("https://cdn.discordapp.com/avatars/374095302648659980/3953362a62cb6a1bdce66f13a31aef4a.png")
-			.addFields(
-				{ name: "Your health:", value: "Your ammo:", inline: true },
-				{ name: "Bot's health:", value: "Bot's ammo:", inline: true },
-				{ name: "\u200B", value: "\u200B" },
-				{ name: "Inline field title", value: "Some value here", inline: true },
-			)
-			.setTimestamp()
-			.setFooter(prefix + this.name + " help for instructions")
-
-		embedMessage.edit(newEmbed);
+		
 	},
 };
