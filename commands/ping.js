b9d4e3767186ea4ced37520e6fce3c2218b804d9
Glitch (moneymoneymoney-bot) <none>
2020-08-28 01:31:34 +0000

    🐴🐘 Checkpoint ./commands/ping.js:1289277/782

--- commands/ping.js
+++ commands/ping.js
@@ -11,20 +11,19 @@ module.exports = {
 	execute(message, arguments){
 		message.channel.send("pong").then(sentMessage => {
 			message.client.pingMessageID = sentMessage;
+			sentMessage.awaitReactions(filter, { max: 4, time: 5000, errors: ['time'] })
+				.then(collected => console.log(collected.size))
+				.catch(collected => {
+					console.log("After a minute, only " + collected.size + " out of 4 reacted.");
+				});
 		});
+
+		const filter = (reaction, user) => {
+			return reaction.emoji.name === "👍" && user.id === message.author.id;
+		};
 
 		if (message.author.id != 134095374381088768){ // If you are not me
             return;
         }
-
-		message.client.pingMessageID.awaitReactions(filter, { max: 4, time: 60000, errors: ['time'] })
-			.then(collected => console.log(collected.size))
-			.catch(collected => {
-				console.log("After a minute, only " + collected.size + " out of 4 reacted.");
-			});
 	},
-};
-
-const filter = (reaction, user) => {
-	return reaction.emoji.name === "👍" && user.id === message.author.id;
 };
