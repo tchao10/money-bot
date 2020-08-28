c05e926ea627b5a50d938280d62df4939737e0e6
Glitch (moneymoneymoney-bot) <none>
2020-08-28 06:47:07 +0000

    💆⛪️ Checkpoint ./commands/ping.js:1289277/570

--- commands/ping.js
+++ commands/ping.js
@@ -12,17 +12,20 @@ module.exports = {
 		const shootIcon = "💥";
 		const reloadIcon = "🔂";
 		const blockIcon = "🛡";
-
-		message.channel.send(shootIcon + " " + reloadIcon + " " + blockIcon);
-		
-		message.channel.send("pong").then(sentMessage => {
+				
+		message.channel.send("pong").then(async sentMessage => {
 			message.client.pingMessageID = sentMessage;
-			sentMessage.react("👍");
+
+			await sentMessage.react(shootIcon);
+			await sentMessage.react(reloadIcon);
+			await sentMessage.react(blockIcon);
+
 			sentMessage.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
 				.then(collected => {
 					const reactedEmoji = collected.first()._emoji.name;
 					console.log(reactedEmoji);
 					sentMessage.reactions.resolve(reactedEmoji).users.remove(message.author);
+					message.channel.send("You reacted with " + reactedEmoji);
 				})
 				.catch(collected => {
 					message.channel.send("You took too long! The game has been stopped.");
@@ -31,7 +34,7 @@ module.exports = {
 		});
 
 		const filter = (reaction, user) => {
-			return reaction.emoji.name === "👍" && user.id === message.author.id;
+			return user.id === message.author.id && (reaction.emoji.name === shootIcon || reaction.emoji.name === reloadIcon || reaction.emoji.name === blockIcon);
 		};
 
 		if (message.author.id != 134095374381088768){ // If you are not me
