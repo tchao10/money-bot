dd9165c82a6a3aa3d26c02d8e8c077c696019ab6
Glitch (moneymoneymoney-bot) <none>
2020-09-01 06:56:50 +0000

    🚌🎯 Checkpoint ./commands/serverinfo.js:1289277/643

---
+++ commands/serverinfo.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: "serverinfo",
+	description: "Displays some information about this server.",
+	usage: "",
+	aliases: ["si"],
+	arguments: false,
+	guildOnly: true,
+	execute(message, arguments) {
+        
+	},
+};
